3a95a4931aa687756465f79b142accefcc403b0f
seanycarol <seany@ice.ufjf.br>
2024-11-20 14:33:24 -0300

    Integration with S3

---
+++ src/main/java/com/rocketseat/createUrlShortener/UrlData.java
@@ -0,0 +1,13 @@
+package com.rocketseat.createUrlShortener;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
